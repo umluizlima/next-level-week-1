5f7c8be6315825030c500f9fbd6f18527aa96a35
Luiz Lima <umluizlima@gmail.com>
2020-06-06 18:44:37 -0300

    Add map

--- frontend/src/pages/CreatePoint/index.tsx
+++ frontend/src/pages/CreatePoint/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+import { Map, Marker, TileLayer } from 'react-leaflet';
 
 import logo from '../../assets/logo.svg';
 import './styles.css';
@@ -41,6 +42,13 @@ const CreatePoint = () => {
             <h2>Endereço</h2>
             <span>Selecione o endereço no mapa</span>
           </legend>
+          <Map center={[-23.5535016, -46.6222736]} zoom={15}>
+            <TileLayer
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+            />
+            <Marker position={[-23.5535016, -46.6222736]} />
+          </Map>
           <div className="field-group">
             <div className="field">
               <label htmlFor="uf">UF</label>
