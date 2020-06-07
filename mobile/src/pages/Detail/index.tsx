e0e000f7ffc1315021db80966b12b56036fae524
Luiz Lima <umluizlima@gmail.com>
2020-06-07 18:31:40 -0300

    Finish Detail page

--- mobile/src/pages/Detail/index.tsx
+++ mobile/src/pages/Detail/index.tsx
@@ -1,8 +1,69 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Image, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Feather as Icon, FontAwesome } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 const Detail = () => {
-  return <View />;
+  const navigation = useNavigation();
+
+  const handleNavigateBack = () => {
+    navigation.goBack();
+  };
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={handleNavigateBack}>
+          <Icon name="arrow-left" size={20} color="#34cb79" />
+        </TouchableOpacity>
+        <Image
+          style={styles.pointImage}
+          source={{
+            uri: 'https://images.unsplash.com/photo-1557224651-e96a58962468?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60'
+          }}
+        />
+        <Text style={styles.pointName}>
+          Mercado
+        </Text>
+        <Text style={styles.pointItems}>
+          Mercado
+        </Text>
+        <View style={styles.address}>
+          <Text style={styles.addressTitle}>
+            Endereço
+          </Text>
+          <Text style={styles.addressContent}>
+            Endereço
+          </Text>
+        </View>
+      </View>
+      <View style={styles.footer}>
+        <RectButton
+          style={styles.button}
+          onPress={() => {}}
+        >
+          <FontAwesome
+            name="whatsapp"
+            size={20}
+            color="#fff"
+          />
+          <Text style={styles.buttonText}>Whatsapp</Text>
+        </RectButton>
+        <RectButton
+          style={styles.button}
+          onPress={() => {}}
+        >
+          <Icon
+            name="mail"
+            size={20}
+            color="#fff"
+          />
+          <Text style={styles.buttonText}>E-mail</Text>
+        </RectButton>
+      </View>
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({
