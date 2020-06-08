54ed51065da68acefe796932b4c098961e138e04
Luiz Lima <umluizlima@gmail.com>
2020-06-07 21:58:24 -0300

    Validate input data

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,3 +1,4 @@
+import { celebrate, Joi } from 'celebrate';
 import express from 'express';
 import multer from 'multer';
 
@@ -12,7 +13,25 @@ const itemsController = new ItemsController();
 routes.get('/items', itemsController.list);
 
 const pointsController = new PointsController();
-routes.post('/points', upload.single('image'), pointsController.create);
+routes.post(
+  '/points',
+  upload.single('image'),
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().required().email(),
+      whatsapp: Joi.string().required(),
+      latitude: Joi.number().required(),
+      longitude: Joi.number().required(),
+      city: Joi.string().required(),
+      uf: Joi.string().required().max(2),
+      items: Joi.string().regex(RegExp('^\\d(,\\d)*$')).required(),
+    })
+  }, {
+    abortEarly: false,
+  }),
+  pointsController.create,
+);
 routes.get('/points', pointsController.list);
 routes.get('/points/:id', pointsController.get);
 
