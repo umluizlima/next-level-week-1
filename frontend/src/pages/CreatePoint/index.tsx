4f05083a1b36231b395ce979e88f41f560d0fff7
Luiz Lima <umluizlima@gmail.com>
2020-06-07 21:42:32 -0300

    Add file upload from frontend

--- frontend/src/pages/CreatePoint/index.tsx
+++ frontend/src/pages/CreatePoint/index.tsx
@@ -42,6 +42,7 @@ const CreatePoint = () => {
     whatsapp: '',
   });
   const [selectedItems, setSelectedItems] = useState<number[]>([]);
+  const [selectedFile, setSelectedFile] = useState<File>();
   const history = useHistory();
 
   useEffect(() => {
@@ -109,9 +110,19 @@ const CreatePoint = () => {
     const [latitude, longitude] = selectedPosition;
     const items = selectedItems;
 
-    const data = {
-      name, email, whatsapp, uf, city, latitude, longitude, items,
-    };
+    const data = new FormData();
+    data.append('name', name);
+    data.append('email', email);
+    data.append('whatsapp', whatsapp);
+    data.append('uf', uf);
+    data.append('city', city);
+    data.append('latitude', String(latitude));
+    data.append('longitude', String(longitude));
+    data.append('items', items.join(','));
+    if (selectedFile) {
+      data.append('image', selectedFile);
+    }
+
     await api.post('points', data);
     history.push('/');
   };
@@ -127,7 +138,7 @@ const CreatePoint = () => {
       </header>
       <form onSubmit={handleSubmit}>
         <h1>Cadastro do ponto de coleta</h1>
-        <DropZone />
+        <DropZone onFileUploaded={setSelectedFile} />
         <fieldset>
           <legend>
             <h2>Dados</h2>
